f72c9c6e3e55436b64423597504e4708080d1ab6
Ali Sufian <AliSufian27@users.noreply.github.com>
2017-10-12 19:44:10 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
+	$servername = "tcp:stms.database.windows.net";
+	$username = "Ali";
+	$password = "Abc@12345";
+	$dbname = "STMS";
+
+$conn = mysqli_connect($servername, $username, $password,$dbname);
+	
+$sql = "INSERT INTO info (Value) VALUES (11)";
+if(mysqli_query($conn,$sql))
+{
+	echo "Done YoY :)";
+}
+else
+{
+	echo "error is ".mysqli_error($con );
+}
 
 ?>
